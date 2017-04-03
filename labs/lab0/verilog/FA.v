6e6ca1b119dc5eeb14bd48e8b0802998153e67c2
Darts <jhonedarts@gmail.com>
2017-04-03 16:13:24 -0300

    lab0

---
+++ labs/lab0/verilog/FA.v
@@ -0,0 +1,25 @@
+//-----------------------------------------------------------------------------
+// UEFS TEC 499
+// Lab 0, 2016.1
+// Module: FA.v
+// Desc: 1-bit Full Adder
+//       You may only use structural verilog in this module.       
+//-----------------------------------------------------------------------------
+module FA(
+    input A, B, Cin,
+    output Sum, Cout
+);
+  // Structural verilog only!
+   
+/********YOUR CODE HERE********/
+   
+wire a_xor_b, a_and_b, axorb_and_Cin;
+	xor (a_xor_b, a, b);
+	xor (Sum, a_xor_b, Cin);
+	and (a_and_b, a, b);
+	and (axorb_and_Cin, a_xor_b, Cin);
+	or (Cout, a_and_b, axorb_and_Cin);
+/********END CODE********/
+
+endmodule
+
---
+++ labs/lab0/verilog/Mux2_1.v
@@ -0,0 +1,35 @@
+//-----------------------------------------------------------------------------
+
+// UEFS TEC 499
+
+// Lab 0, 2016.1
+
+// Module: Mux2_1.v
+
+// Desc: OUT = A*(~SEL) + B*(SEL)
+
+//-----------------------------------------------------------------------------
+
+ module Mux2_1(
+    input A,
+    input B,
+    input SEL,
+    output OUT
+);
+// You may only use structural verilog! (i.e. wires and gates only)
+ 
+ /********YOUR CODE HERE********/
+
+	wire a_and_Sel1, b_and_Sel0;
+	and (a_and_Sel1, A, ~SEL);
+	and (b_and_Sel0, B, SEL);
+	or ( OUT, a_and_Sel1, b_and_Sel0);
+
+
+
+/********END CODE********/
+  
+// assign OUT = 1'b0; //delete this line once you finish writing your logic
+   
+
+endmodule
