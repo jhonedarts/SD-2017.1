9b9b1ba602f6bb8cf915586cba830e96dec8dc4e
Leocampos1 <leocamposouza@gmail.com>
2017-04-05 22:54:25 -0300

    Update primos.asm

--- assembly codes/primos.asm
+++ assembly codes/primos.asm
@@ -1,11 +1,11 @@
 .module primos
 	.text
 	
-		li $s4, 15						#QUANTIDADE DE NUMEROS PRIMOS(entrada)
-		li $s0, 2						#dividendo
-		move $s1, $s0					#divisor
-		li $s3, 2						#verifica se e primo(apenas duas divisoes com resto 0)
-		move $t2, $gp					#pega o valor do gp para armazenar os numeros primos
+		movi $s4, 15						#QUANTIDADE DE NUMEROS PRIMOS(entrada)
+		movi $s0, 2						#dividendo
+		mov  $s1, $s0					#divisor
+		movi $s3, 2						#verifica se e primo(apenas duas divisoes com resto 0)
+		mov $t2, $gp					#pega o valor do gp para armazenar os numeros primos
 				
 		LOOP:
 			div $s0, $s1				#(dividendo/divisor) atuais
