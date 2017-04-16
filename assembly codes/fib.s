89efc816070076c3e22a388dd2c13a86deb1ee7b
Jhone Darts <jhonedarts@gmail.com>
2017-04-16 15:40:11 -0300

    Fibonacci working
    Fibonacci funcionando, sem a E/S ainda

--- assembly codes/fib.s
+++ assembly codes/fib.s
@@ -1,34 +1,86 @@
-.text
+.text 
 	.global main
-	main:
-		movi r17, 2 					#QUANTIDADE DE NUMEROS NA SEQUENCIA DE FIB
-		#mov r10, $gp				#Endereco de armazenamento
-		movi r18, 1
-		
-		fib:
-		subi r27,r27,12         # save registers on stack
-	        stw r17, 0(r27)          # save r17 = n
-	        stw r16, 4(r27)          # save r16
-	        stw r31, 8(r27)          # save return address r31
-	        bgt r17,r18, gen          # if n>1 then goto generic case
-	        mov r2,r17            # output = input if n=0 or n=1
-	        br rreg                  # goto restore registers
-		
-		gen:
-		subi r17,r17,1           # param = n-1
-	        call fib                 # compute fib(n-1)
-	        mov r16,r2            # save fib(n-1)
-	        subi r17,r17,1           # set param to n-2
-	        call fib                 # and make recursive call
-	        add r2, r2, r16       # r2 = fib(n-2)+fib(n-1)
-		
-		rreg:
-		ldw  r17, 0(r27)         # restore registers from stack
-	        ldw  r16, 4(r27)         #
-	        ldw  r31, 8(r27)         #
-	        addi r27, r27, 12       # decrease the stack size
-	        jmp r31
-		
-		
-			
+	
+main:
+	movi r20, 20		#r20 = limite da pilha, dá estouro de pilha no 13º
+	movia r21, saida	#guarda o endereço das saidas em r21
+	movi r16,0
+	#movi r2,4
+	#movia r4,msg
+	#call printf
+	movi r2,15		#valor de entrada
+	#call scanf
+	bge r2,r20,end		#if n<=20
+	movi r17,1		#c=1
+	mov r18,r2		#n
+loop:	
+	mov r4,r16		#mov i -> int i
+	bgt r17,r18,end		#if c<=n
+	call fib
+	mov r4,r2		#mov return -> r4 to print
+	movi r2,1	
+	#call printf		#print number
+	stw r4, 0(r21)
+	addi r21,r21,4 
+	#movia r4,msg2
+	#movi r2,4
+	#call printf		#print space
+	addi r17,r17,1		#c=c+1
+	addi r16,r16,1		#i=i+1
+	br loop
+
+
+
+##################################
+
+ fib:
+	addi r27, r27, -12
+	stw r31, 8(r27)
+	stw r16, 4(r27)
+	addi r2, r0, 1
+	beq r4, r0, fin2
+	addi r8, r0, 1
+	beq r4, r8, fin
+	addi r4, r4, -1
+	stw r4, 0(r27)
+	call fib
+	add r16, r2, r0
+	ldw r4, 0(r27)
+	addi r4, r4, -1
+	call fib
+	add r2, r2, r16
+fin:
+	ldw r16, 4(r27)
+	ldw r31, 8(r27)
+	addi r27, r27, 12
+	jmp r31
+	
+fin2:				#necessário criar esta label para retornar 0
+	movi r2,0		
+	ldw r16, 4(r27)
+	ldw r31, 8(r27)
+	addi r27, r27, 12
+	jmp r31
+	
+end:
+	movi r2,10
+	#call printf
 .data
+#msg:	.asciz "Insira um numero: "
+#msg2:	.asciz " "
+saida:
+.word 0
+.word 0
+.word 0
+.word 0
+.word 0
+.word 0
+.word 0
+.word 0
+.word 0
+.word 0
+.word 0
+.word 0
+.word 0
+.word 0
+.word 0
