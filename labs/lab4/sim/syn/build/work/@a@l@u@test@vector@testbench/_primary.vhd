0b1e6ee15ef925106b88a75f4d9f26a4b20c6e06
Leocampos1 <leocamposouza@gmail.com>
2017-08-15 10:00:58 -0300

    Lab4

---
+++ labs/lab4/sim/syn/build/work/@a@l@u@test@vector@testbench/_primary.vhd
@@ -0,0 +1,9 @@
+library verilog;
+use verilog.vl_types.all;
+entity ALUTestVectorTestbench is
+    generic(
+        Halfcycle       : real    := 8.200000
+    );
+    attribute mti_svvh_generic_type : integer;
+    attribute mti_svvh_generic_type of Halfcycle : constant is 1;
+end ALUTestVectorTestbench;
